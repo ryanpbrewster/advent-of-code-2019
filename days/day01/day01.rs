c79a180c373ef9c2ed582828c9df66a9be3a5d4e
Ryan P. Brewster <RyanPBrewster@gmail.com>
2019-12-20 00:36:01 -0800

    add rustfmt

--- days/day01/day01.rs
+++ days/day01/day01.rs
@@ -2,43 +2,42 @@ use std::fs;
 
 #[test]
 fn part1() {
-  let contents = fs::read_to_string("days/day01/input.txt").expect("read input file");
-  let total_fuel: i32 = contents
-      .lines()
-      .map(|s| fuel_for_module(s.parse().expect("parse line as i32")))
-      .sum();
+    let contents = fs::read_to_string("days/day01/input.txt").expect("read input file");
+    let total_fuel: i32 = contents
+        .lines()
+        .map(|s| fuel_for_module(s.parse().expect("parse line as i32")))
+        .sum();
 
-  assert_eq!(total_fuel, 3305301);
+    assert_eq!(total_fuel, 3305301);
 }
 
 #[test]
 fn part2_smoke() {
-  assert_eq!(meta_fuel_for_module(14), 2);
-  assert_eq!(meta_fuel_for_module(1969), 966);
-
+    assert_eq!(meta_fuel_for_module(14), 2);
+    assert_eq!(meta_fuel_for_module(1969), 966);
 }
 
 #[test]
 fn part2() {
-  let contents = fs::read_to_string("days/day01/input.txt").expect("read input file");
-  let total_fuel: i32 = contents
-      .lines()
-      .map(|s| meta_fuel_for_module(s.parse().expect("parse line as i32")))
-      .sum();
+    let contents = fs::read_to_string("days/day01/input.txt").expect("read input file");
+    let total_fuel: i32 = contents
+        .lines()
+        .map(|s| meta_fuel_for_module(s.parse().expect("parse line as i32")))
+        .sum();
 
-  assert_eq!(total_fuel, 4955106);
+    assert_eq!(total_fuel, 4955106);
 }
 
 fn fuel_for_module(mass: i32) -> i32 {
-  mass / 3 - 2
+    mass / 3 - 2
 }
 
 fn meta_fuel_for_module(mass: i32) -> i32 {
-  let mut total = 0;
-  let mut cur = mass;
-  while cur > 0 {
-    cur = std::cmp::max(fuel_for_module(cur), 0);
-    total += cur;
-  }
-  total
+    let mut total = 0;
+    let mut cur = mass;
+    while cur > 0 {
+        cur = std::cmp::max(fuel_for_module(cur), 0);
+        total += cur;
+    }
+    total
 }
---
+++ justfile
@@ -0,0 +1,2 @@
+fmt:
+  find . -name "*.rs" -exec rustfmt {} +
