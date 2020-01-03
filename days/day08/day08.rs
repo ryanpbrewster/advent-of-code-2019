efcfea07c175d0453c642151d07479b594d27f40
Ryan P. Brewster <RyanPBrewster@gmail.com>
2020-01-03 01:25:21 -0800

    clean up enum for day8

--- days/day08/day08.rs
+++ days/day08/day08.rs
@@ -1,80 +1,62 @@
 extern crate files;
 
-use std::convert::From;
-use std::fmt;
-
 const HEIGHT: usize = 6;
 const WIDTH: usize = 25;
 const SIZE: usize = WIDTH * HEIGHT;
 
-#[derive(Debug, Eq, PartialEq, Copy, Clone)]
-enum Color {
-    Black,
-    White,
-    Clear,
-}
-impl From<u8> for Color {
-    fn from(b: u8) -> Color {
-        match b {
-            b'0' => Color::Black,
-            b'1' => Color::White,
-            _ => Color::Clear,
-        }
-    }
-}
-impl fmt::Display for Color {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match *self {
-            Color::White => f.write_str("\x1B[47m \x1B[0m"),
-            Color::Black => f.write_str("\x1B[40m \x1B[0m"),
-            Color::Clear => f.write_str(" "),
-        }
-    }
-}
-
 #[test]
 fn part1() {
     let input = files::read!("input.txt");
-    let layers: Vec<Vec<Color>> = input
+    let layers: Vec<Vec<u8>> = input
+        .trim()
         .as_bytes()
-        .chunks_exact(SIZE)
-        .map(|bs| bs.iter().map(|&b| Color::from(b)).collect())
+        .chunks(SIZE)
+        .map(|bs| bs.to_vec())
         .collect();
     let best = layers
         .into_iter()
-        .min_by_key(|layer| layer.iter().filter(|&&c| c == Color::Black).count())
+        .min_by_key(|layer| layer.iter().filter(|&&b| b == b'0').count())
         .unwrap();
-    let whites = best.iter().filter(|&&c| c == Color::White).count();
-    let clears = best.iter().filter(|&&c| c == Color::Clear).count();
-    assert_eq!(whites * clears, 2193);
+    let ones = best.iter().filter(|&&b| b == b'1').count();
+    let twos = best.iter().filter(|&&b| b == b'2').count();
+    assert_eq!(ones * twos, 2193);
 }
 
 #[test]
 fn part2() {
     let input = files::read!("input.txt");
-    let layers: Vec<Vec<Color>> = input
+    let layers: Vec<Vec<u8>> = input
+        .trim()
         .as_bytes()
-        .chunks_exact(SIZE)
-        .map(|bs| bs.iter().map(|&b| Color::from(b)).collect())
+        .chunks(SIZE)
+        .map(|bs| bs.to_vec())
         .collect();
-    let stacked: Vec<Color> = (0..SIZE)
+    let stacked: Vec<u8> = (0..SIZE)
         .map(|i| first(layers.iter().map(|layer| layer[i])))
         .collect();
     for line in stacked.chunks_exact(WIDTH) {
-        for c in line {
-            print!("{}", c);
-        }
-        println!();
+        printline(line)
     }
     // This prints out "YEHEF"
-    // panic!()
+    panic!()
 }
 
-fn first(xs: impl Iterator<Item = Color>) -> Color {
+fn first(xs: impl Iterator<Item = u8>) -> u8 {
     for x in xs {
-        if x != Color::Clear {
+        if x != b'2' {
             return x;
         }
     }
-    Color::Clear
+    b'2'
+}
+
+fn printline(line: &[u8]) {
+    for &b in line {
+        match b {
+            b'0' => print!("\x1B[47m "),
+            b'1' => print!("\x1B[40m "),
+            _ => print!("\x1B[0m "),
+        };
+    }
+    println!("\x1B[0m");
 }
